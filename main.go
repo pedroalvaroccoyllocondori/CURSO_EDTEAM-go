c1a839ee49b5079c24b7e11d150248bf406bc4c5
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-13 17:28:13 -0500

    operadores de asignacion y  aritmeticos

--- main.go
+++ main.go
@@ -3,49 +3,18 @@ package main
 import "fmt"
 
 func main() {
-
-	// bool,strin, numeric
-	var booleano bool = true
-	var cadena string = "hola mundo"
-	var positivo uint8 = 255
-	var bite byte = 255      //alias de uint8
-	var negativo rune = -100 //alias de int32
-	var caracter rune = 'a'  //alias de unicode code point
-	//comillas simples para un solo caracter
-	var flotante float64 = 23.56
-
-	//numerioco uint int positivos y negativos respectivamente
-	//imprimir el tipo de dato y el valor de la variable
-	fmt.Printf("tipo:%T,valor:%v", booleano, booleano) //metodo para imprimir datos
-	fmt.Println("")
-	fmt.Printf("tipo:%T,valor:%v", cadena, cadena) //metodo para imprimir datos
-	fmt.Println("")
-	fmt.Printf("tipo:%T,valor:%v", positivo, positivo) //metodo para imprimir datos
-	fmt.Println("")
-	fmt.Printf("tipo:%T,valor:%v", bite, bite) //metodo para imprimir datos
-	fmt.Println("")
-	fmt.Printf("tipo:%T,valor:%v", negativo, negativo) //metodo para imprimir datos
-	fmt.Println("")
-	fmt.Printf("tipo:%T,valor:%v", caracter, caracter) //metodo para imprimir datos
-	fmt.Println("")
-	fmt.Printf("tipo:%T,valor:%v", flotante, flotante) //metodo para imprimir datos
-
-	// no se pueden ejecutar operaciones con diferente tipo de dato
-	resultado := float64(positivo) + flotante // castin para tipo de dato
-	// no se cambian los tipos de datos a pesar del casting
-	fmt.Println("")
-	fmt.Printf("tipo:%T,valor:%v", resultado, resultado) //metodo para imprimir datos
-
-	//operador blank en go
-	// en go toda variable declarada tiene que ser usada
-	// para dejar variables declaradas que no queremos usar usamos el operador blank
-	_ = 230
-	var _ string = "prueba"
-
-	//asignacion automatica
-
-	var vacio bool //le asgina el operador nulo encaso de string es un nulo uint es cero
-	// en caso de flotantes el operador automaticamente asignado es false
-	fmt.Printf("tipo:%T,valor:%v", vacio, vacio) //metodo para imprimir datos
+	//operadores aritmeticos (),*,/,%,+,-
+	var a = 4 + 2
+	fmt.Println(a)
+	//operadores de asignacion =,-=,+=,/=,%=
+	var b = 10
+	b += 2
+	fmt.Println(b)
+	// declaracion post-incremento y post-decremento: ++ , --
+	// no hay predecremento y predecrecimiento
+	// no son una expresion sino una declaracion
+	var c = 3
+	c--
+	fmt.Println(c) //no se puede declarar en el print # ojo
 
 }
