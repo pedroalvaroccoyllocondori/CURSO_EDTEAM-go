f576e017e9bff490fff6fd17d0ac5847206be4fe
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-13 17:48:50 -0500

    operadores de comparacion y logicos

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	//operadores aritmeticos (),*,/,%,+,-
-	var a = 4 + 2
-	fmt.Println(a)
-	//operadores de asignacion =,-=,+=,/=,%=
-	var b = 10
-	b += 2
-	fmt.Println(b)
-	// declaracion post-incremento y post-decremento: ++ , --
-	// no hay predecremento y predecrecimiento
-	// no son una expresion sino una declaracion
-	var c = 3
-	c--
-	fmt.Println(c) //no se puede declarar en el print # ojo
+	//operadores  de comparacion: >,<,==,!= ,>=,<=
+	fmt.Println(4 > 5)
+	fmt.Println(4 != 5)
+	fmt.Println(4 == 4)
+	//operadores logicos
+	var edad = 25
+	fmt.Println(edad >= 18 && edad <= 60)
+
+	fmt.Println(edad <= 18 || edad >= 60)
+	//operadores unarios :!
+	fmt.Println(!(4 != 4))
 
 }
