21c0dcd89ac7cb7a8bd4db7e19d77b1f9a4a3d9a
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-13 22:46:11 -0500

    slices o apuntadores

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import "fmt"
 
 func main() {
 
-	//declaracion de  un array
-	var comida [3]string
-	comida[0] = "🍕"
-	comida[1] = "🍔"
-	comida[2] = "🍗"
-	fmt.Println(comida)
-	//declaracion con asignacion
-	frutas := [3]string{"🍇", "🍌", "🍉"}
-	fmt.Println(frutas)
+	//los slices no poseen datos , son apuntadores a un array
+	conjunto := [5]string{"🐎", "😸", "🦃", "🐢", "🐄"}
+	//slice el ultimo elemeto es excluyente
+	//los slices toman partes del array
+	animales := conjunto[0:3]
+	voladores := conjunto[3:5] // si no ponemos el segundo parametro se rellena con el ultimo .ejemplo= [3:]
+	voladores[0] = "🦋"
+	fmt.Println(conjunto)
+	fmt.Println(animales)
+	fmt.Println(voladores)
+	fmt.Println(voladores[0])
+	fmt.Println(conjunto[:]) // si no especificamos el numero se autoreellena con todos los elemtos
 
 }
