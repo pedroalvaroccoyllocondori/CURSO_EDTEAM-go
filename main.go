ed37c8976b6a346f40f8962bfadb39ad96a5c7e8
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-13 23:52:34 -0500

    slice 2

--- main.go
+++ main.go
@@ -4,17 +4,15 @@ import "fmt"
 
 func main() {
 
-	//los slices no poseen datos , son apuntadores a un array
-	conjunto := [5]string{"🐎", "😸", "🦃", "🐢", "🐄"}
-	//slice el ultimo elemeto es excluyente
-	//los slices toman partes del array
-	animales := conjunto[0:3]
-	voladores := conjunto[3:5] // si no ponemos el segundo parametro se rellena con el ultimo .ejemplo= [3:]
-	voladores[0] = "🦋"
-	fmt.Println(conjunto)
-	fmt.Println(animales)
-	fmt.Println(voladores)
-	fmt.Println(voladores[0])
-	fmt.Println(conjunto[:]) // si no especificamos el numero se autoreellena con todos los elemtos
+	comida := [5]string{"🌭", "🍓", "🍋", "🍔", "🍕"}
+
+	frutas := comida[1:3]
+	frutas = append(frutas, "🍍", "🍈", "🍐")
+
+	fmt.Println("comidas", comida)
+	fmt.Println("frutas", frutas)
+
+	fmt.Println("tamaño frutas", len(frutas))
+	fmt.Println("capacidad frutas", cap(frutas))
 
 }
