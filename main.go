36fae5d38268f658f4173ffb414e120cd1fe26b5
unknown <pedro_alvaro@outlook.com.pe>
2020-07-16 18:41:07 -0500

    for range en slices(estructuras de datos similares a arrays)

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import "fmt"
 func main() {
 	//en go solo existeel for
 
-	//for foerever es un ciclo que dura para siempre
+	//for range slice funciona npara iterar slices
 
-	i := 0
-	for {
-		fmt.Println("ciclo", i)
-		i++
+	numeros := []uint8{2, 4, 6, 8, 10}
 
+	for indice, valor := range numeros {
+		fmt.Println("indice", indice, "valor", valor)
 	}
+
+	nums := []uint8{2, 4, 6, 8, 10}
+	for _, valor := range nums { // no cambia el valor ya que  se esta utilizando una opia del elemeto
+		valor *= 5
+	}
+	fmt.Println(nums)
+
+	for indice, _ := range nums {
+		nums[indice] *= 5
+	}
+	fmt.Println(nums)
 
 }
