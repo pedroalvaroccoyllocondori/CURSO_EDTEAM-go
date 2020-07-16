4aa241ffb68e07bf0067dcec116476a31b49d449
unknown <pedro_alvaro@outlook.com.pe>
2020-07-16 18:16:15 -0500

    for clasico en go

--- main.go
+++ main.go
@@ -3,31 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	emoji := "😺"
-	switch emoji {
-	case "😺": // no es necesario usar la palabra break
-		fmt.Println("es un gato")
-	case "🐶", "🐕": //case de forma multiple
-		fmt.Println("es un un perro")
-	case "🐦", "🦅": // case de forma multiple
-		fmt.Println("es un ave")
+	//en go solo existeel for
 
-	default:
-		fmt.Println(" no es ni gato ni perro")
+	for i := 0; i < 10; i++ {
+		fmt.Println("ciclo", i)
 
 	}
 
-	//swit con estructura logica
-	fruta := "🍎"
-	switch {
-	case fruta == "🍎" || fruta == "🍍": // no es necesario usar la palabra break
-		fmt.Println("es una fruta")
-
-	case fruta == "🐦" || fruta == "🦅": // case de forma multiple
-		fmt.Println("no es una fruta")
-
-	default:
-		fmt.Println(" no es ni fruta ni nada de o anterior")
-
-	}
 }
