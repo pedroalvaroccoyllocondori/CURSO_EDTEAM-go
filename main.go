37579b4fa6f20ff084bf1c3af3b556b82f06901c
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-17 11:27:06 -0500

    funciones en go

--- main.go
+++ main.go
@@ -3,18 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//en go solo existeel for
+	// funciones en go
+	hola()
 
-	//for range  en tipode datos string
+}
 
-	hola := "hola a todos"
-
-	for _, valor := range hola {
-		fmt.Println(valor) //si defrente imprimins  los valores de las letras no dan los bites de cada letra
-
-	}
-	for _, valor := range hola {
-		fmt.Println(string(valor)) //para obtener los valores debemos de  hacer un castin  a los bites de cada letra
-	}
-
+func hola() {
+	fmt.Println("hola a todos mis amigos en go ")
 }
