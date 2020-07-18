9c0153e12068f14a7d4593cdee22b39139d18628
unknown <pedro_alvaro@outlook.com.pe>
2020-07-18 15:14:22 -0500

    funciones que devuelven otras  funciones

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	// errores en go
+	numeros := []int{1, 2, 3, 4, 5, 6, 7, 8, 200, 300, 4000}
+	resultados := filtrar(numeros, func(numeros int) bool {
+		if numeros <= 10 {
+			return true
 
-	resultado, errorr := division(10, 5)
-	if errorr != nil {
-		fmt.Printf("ocurrio un error: %v", errorr)
-		return // para finalizar la ejecucion de el programa
-	}
-	fmt.Println("la respuesta es :", resultado)
+		}
+		return false
+
+	})
+	fmt.Println(resultados)
 
 }
+func filtrar(numeros []int, funcion func(int) bool) []int {
 
-func division(dividendo, divisor int) (resultado int, err error) {
+	resultado := []int{}
+	for _, valor := range numeros {
+		if funcion(valor) {
+			resultado = append(resultado, valor)
 
-	if divisor == 0 {
-		err = errors.New("no puedes dividir entre cero")
-		return //retornara valor cero
+		}
 	}
-	resultado = dividendo / divisor
-	return //retornara el valor cero de error osea (nil)
+	return resultado
+
 }
