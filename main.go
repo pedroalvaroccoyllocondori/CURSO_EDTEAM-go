c6ce5eb6a7ec8922890b03cffa3d79b5e92bf034
unknown <pedro_alvaro@outlook.com.pe>
2020-07-18 15:22:48 -0500

    funciones que devuelven otrasfunciones parte 2

--- main.go
+++ main.go
@@ -4,27 +4,14 @@ import "fmt"
 
 func main() {
 	// errores en go
-	numeros := []int{1, 2, 3, 4, 5, 6, 7, 8, 200, 300, 4000}
-	resultados := filtrar(numeros, func(numeros int) bool {
-		if numeros <= 10 {
-			return true
 
-		}
-		return false
-
-	})
-	fmt.Println(resultados)
+	saludo := hola("alvaro")("ccoyllo")
+	fmt.Println(saludo)
 
 }
-func filtrar(numeros []int, funcion func(int) bool) []int {
 
-	resultado := []int{}
-	for _, valor := range numeros {
-		if funcion(valor) {
-			resultado = append(resultado, valor)
-
-		}
+func hola(nombre string) func(string) string {
+	return func(apellido string) string {
+		return "hola " + nombre + " " + apellido
 	}
-	return resultado
-
 }
