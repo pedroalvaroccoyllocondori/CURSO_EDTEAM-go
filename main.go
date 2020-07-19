ae20e82d37fc388706f99a17b11f452e17bddcaa
unknown <pedro_alvaro@outlook.com.pe>
2020-07-18 22:48:19 -0500

    defer o (aplazador) parte 2 pilas

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	// defer en go
 	// nos permite diferir algo  o aplzara la ejecucionde una funcion
-	defer fmt.Println(3) // se ejecuta al finalizar todas la lineas
-
-	fmt.Println(1)
-	fmt.Println(2)
+	defer fmt.Println(3)
+	defer fmt.Println(2)
+	defer fmt.Println(1)
+	//en este caso se agregan a la pila  el  1 despues  el 2 desapues el 3
+	// se imprimen del primero al ultimo elemto en la pila
 
 }
